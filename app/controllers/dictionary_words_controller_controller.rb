177d03814694a517a2046c647b96d27ee40cf870
Kevin Rosengren <kev.rosengren@gmail.com>
2016-01-28 09:58:42 -0500

    working step 4.4

--- app/controllers/dictionary_words_controller_controller.rb
+++ app/controllers/dictionary_words_controller_controller.rb
@@ -1,5 +1,17 @@
+require 'json'
+require "#{Rails.root}/app/models/web_spellchecker.rb"
+
 class DictionaryWordsControllerController < ApplicationController
+  respond_to :json
+
   def spellcheck
-      @term = params[:term]
+      spellChecker = WebSpellchecker.new
+      #@terms = spellChecker.correct(params[:term])
+      hash = {
+        :term => params[:term],
+        :known => true,
+        :suggestions => spellChecker.correct(params[:term])}
+
+      render json: hash
   end
 end
---
+++ app/models/web_spellchecker.rb
@@ -0,0 +1,10 @@
+class WebSpellchecker < Spellchecker
+
+  def initialize
+
+  end
+
+  def correct(w)
+    return ["testing", "purposes"]
+  end
+end
